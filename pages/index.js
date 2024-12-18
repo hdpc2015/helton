e9074215dc5d73c8970577121d5db02ccc5d533b
hdpc2015 <46406671+hdpc2015@users.noreply.github.com>
2024-12-18 22:26:06 +0000

    adicionar página `Em construção`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-function Home() {
-  return <h1>Helton Tibiano!</h1>;
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f4f4f9",
+        fontFamily: "Arial, sans-serif",
+        margin: 0,
+      }}
+    >
+      <h1 style={{ margin: "10px 0", fontSize: "1.8rem", color: "#333" }}>
+        Em construção...
+      </h1>
+      <img
+        src="https://i.imgur.com/CX5naKG.jpeg"
+        alt="Imagem em construção"
+        style={{ width: "700px", height: "auto", marginTop: "10px" }}
+      />
+    </div>
+  );
 }
-
-export default Home;
